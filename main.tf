987cd30f19debed5892afc92ffb8fd9494f2c55e
t-xxx <happycatcat23@gmail.com>
2023-05-31 22:40:33 +0900

    Deleted terraform option and added provider option

--- main.tf
+++ main.tf
@@ -1,10 +1,11 @@
-terraform {
-  required_version = "~> 1.4.6"
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-    }
-  }
+variable "access_key" {}
+
+variable "secret_key" {}
+
+provider "aws" {
+  region                  = "ap-northeast-1"
+  access_key = var.access_key
+  secret_key = var.secret_key
 }
 
 provider "aws" {
